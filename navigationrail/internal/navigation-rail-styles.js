e41076edffc939380537e75307b7fe50cc43b478
treeder <treeder@gmail.com>
2024-06-04 18:00:13 +0000

    Added navigation rail

---
+++ navigationrail/internal/navigation-rail-styles.js
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// Generated stylesheet for ./labs/navigationbar/internal/navigation-bar-styles.css.
+import { css } from 'lit'
+export const styles = css`
+
+.md3-navigation-rail {
+    flex-direction: column;
+    width: 80px;
+}
+`
+//# sourceMappingURL=navigation-bar-styles.js.map
