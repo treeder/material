08f4cdc9513d1fad5b51340d1b0fd9dc038f3d31
Travis Reeder <treeder@gmail.com>
2024-06-04 14:02:15 -0400

    Added navigation rail (#17)

---
+++ navigationrail/navigation-rail.js
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib"
+import { customElement } from 'lit/decorators.js'
+import { NavigationBar } from './internal/navigation-rail.js'
+import { styles } from './internal/navigation-rail-styles.js'
+/**
+ * @final
+ * @suppress {visibility}
+ */
+export let MdNavigationRail = class MdNavigationRail extends NavigationBar {
+}
+MdNavigationRail.styles = [styles]
+MdNavigationRail = __decorate([
+    customElement('md-navigation-rail')
+], MdNavigationRail)
+//# sourceMappingURL=navigation-rail.js.map
